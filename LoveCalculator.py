fed2d69db125aa20774a60a87e8434fc35c8847e
Ruthik <ruthik1231@gmail.com>
2023-05-24 17:00:02 +0530

    quickupdate

---
+++ LoveCalculator.py
@@ -0,0 +1,28 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+n1=name1.lower()
+n2=name2.lower()
+n3=n1+n2
+t=n3.count("t")
+r=n3.count("r")
+u=n3.count("u")
+e=n3.count("e")
+true=t+r+u+e
+l=n3.count("l")
+o=n3.count("o")
+v=n3.count("v")
+e=n3.count("e")
+love=l+o+v+e
+love_score=str(true)+str(love)
+love_score1=int(love_score)
+if love_score1<10 or love_score1>90:
+    print(f"Your score is {love_score1},you go together like coke and mentos.")
+elif love_score1>40 and love_score1<50:
+    print(f"Your score is {love_score1},you are alright together.")
+else:
+    print(f"Your score is {love_score1}.")
--- PasswordGenarator.py
+++ PasswordGenarator.py
@@ -37,7 +37,7 @@ for char in range(1, nr_numbers + 1):
 
 print(password_list)
 random.shuffle(password_list)
-print(password_list)
+#print(password_list)
 
 password = ""
 for char in password_list:
